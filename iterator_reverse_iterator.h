698ddc02b4e657a7c47df2f356a1893c29d4404a
yuche <yuchenfengjs@163.com>
2021-04-14 17:18:08 +0800

    fix samll bug

--- iterator_reverse_iterator.h
+++ iterator_reverse_iterator.h
@@ -2,7 +2,7 @@
  * @Description: 迭代器适配器。reverse_iterator。可以将一般迭代器的行进方向逆向，使原本应该前进的 operator++ 变为了后退操作。operator-- 变为了前进操作。
  * @Author: Chen.Yu
  * @Date: 2021-04-02 14:50:09
- * @LastEditTime: 2021-04-10 21:47:58
+ * @LastEditTime: 2021-04-14 17:16:53
  * @LastEditors: Chen.Yu
  */
 #ifndef _ITERATOR_REVERSE_ITERATOR_H
@@ -44,6 +44,7 @@ namespace MySTL {
         // TO DO
         reference operator*() const {
             return *(current_ - 1);
+            // 以上比较关键，堆逆向迭代器取值，就是将“对应正向迭代器”后退一格而后取值
         }
 
         pointer operator->() const {
