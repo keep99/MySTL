2048a0dd56be30ae774014752e8ec7a238a379f6
yuche <yuchenfengjs@163.com>
2021-05-16 04:34:53 +0800

    Fix format error

--- src/tempbuf.h
+++ src/tempbuf.h
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Chen.Yu
  * @Date: 2021-05-16 04:25:10
- * @LastEditTime: 2021-05-16 04:32:45
+ * @LastEditTime: 2021-05-16 04:34:28
  * @LastEditors: Chen.Yu
  */
 #ifndef _TEMPBUF_H_
@@ -24,36 +24,36 @@ namespace MySTL {
     class temporary_buffer
     {
     private:
-    ptrdiff_t original_len;  // 缓冲区申请的大小
-    ptrdiff_t len;           // 缓冲区实际的大小
-    T*        buffer;        // 指向缓冲区的指针
+        ptrdiff_t original_len;  // 缓冲区申请的大小
+        ptrdiff_t len;           // 缓冲区实际的大小
+        T*        buffer;        // 指向缓冲区的指针
 
     public:
-    // 构造、析构函数
-    temporary_buffer(ForwardIterator first, ForwardIterator last);
+        // 构造、析构函数
+        temporary_buffer(ForwardIterator first, ForwardIterator last);
 
-    ~temporary_buffer()
-    {
-        MySTL::destroy(buffer, buffer + len);
-        free(buffer);
-    }
+        ~temporary_buffer()
+        {
+            MySTL::destroy(buffer, buffer + len);
+            free(buffer);
+        }
 
     public:
 
-    ptrdiff_t size()           const noexcept { return len; }
-    ptrdiff_t requested_size() const noexcept { return original_len; }
-    T*        begin()                noexcept { return buffer; }
-    T*        end()                  noexcept { return buffer + len; }
+        ptrdiff_t size()           const noexcept { return len; }
+        ptrdiff_t requested_size() const noexcept { return original_len; }
+        T*        begin()                noexcept { return buffer; }
+        T*        end()                  noexcept { return buffer + len; }
 
     private:
-    void allocate_buffer();
-    void initialize_buffer(const T&, std::true_type) {}
-    void initialize_buffer(const T& value, std::false_type)
-    { MySTL::uninitialized_fill_n(buffer, len, value); }
+        void allocate_buffer();
+        void initialize_buffer(const T&, std::true_type) {}
+        void initialize_buffer(const T& value, std::false_type)
+        { MySTL::uninitialized_fill_n(buffer, len, value); }
 
     private:
-    temporary_buffer(const temporary_buffer&);
-    void operator=(const temporary_buffer&);
+        temporary_buffer(const temporary_buffer&);
+        void operator=(const temporary_buffer&);
     };
 
     // 构造函数
@@ -82,16 +82,16 @@ namespace MySTL {
     template <class ForwardIterator, class T>
     void temporary_buffer<ForwardIterator, T>::allocate_buffer()
     {
-    original_len = len;
-    if (len > static_cast<ptrdiff_t>(INT_MAX / sizeof(T)))
-        len = INT_MAX / sizeof(T);
-    while (len > 0)
-    {
-        buffer = static_cast<T*>(malloc(len * sizeof(T)));
-        if (buffer)
-        break;
-        len /= 2;  // 申请失败时减少申请空间大小
-    }
+        original_len = len;
+        if (len > static_cast<ptrdiff_t>(INT_MAX / sizeof(T)))
+            len = INT_MAX / sizeof(T);
+        while (len > 0)
+        {
+            buffer = static_cast<T*>(malloc(len * sizeof(T)));
+            if (buffer)
+            break;
+            len /= 2;  // 申请失败时减少申请空间大小
+        }
     }
 }
 
